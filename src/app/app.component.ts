9a1db5a6a6cfdda87461eac977743c3bb227ca34
NerphMe <dr0n74085@gmail.com>
2021-05-11 01:31:31 +0300

    Todo List

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,73 @@
-import { Component } from '@angular/core';
+// noinspection NegatedConditionalExpressionJS,TypeScriptUnresolvedVariable
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Todo} from './todo';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'my-app';
+export class AppComponent implements OnInit {
+  public title = '';
+  public childTitle = '';
+  public toggle = [];
+  public todoList: Todo[];
+  private httpClint: HttpClient;
+
+  constructor(httpClient: HttpClient) {
+    this.httpClint = httpClient;
+  }
+
+  ngOnInit(): void {
+    this.httpClint.get<Todo[]>('http://127.0.0.1:8000/api/todos')
+      .subscribe(todoList => {
+        this.todoList = todoList;
+      });
+  }
+
+  onCreate(): void {
+    if (this.title) {
+      this.httpClint
+        .post<Todo[]>('http://127.0.0.1:8000/api/create/todo', {title: this.title})
+        .subscribe(todo => {
+            // @ts-ignore
+            return this.todoList.push(todo);
+          }
+        );
+      this.title = '';
+    }
+  }
+
+  onCreateChildren(createChildren: Todo): void {
+    if (this.childTitle) {
+      this.httpClint
+        .post<Todo[]>('http://127.0.0.1:8000/api/create/children/todo', {title: this.childTitle, parent_id: createChildren.id})
+        .subscribe(todo => {
+            // @ts-ignore
+          this.todoList = this.todoList.push(todo);
+          }
+        );
+      this.childTitle = '';
+    }
+  }
+
+  onRemove(todoOnDelete: Todo): void {
+    this.httpClint
+      .delete<void>('http://127.0.0.1:8000/api/delete/todo/' + todoOnDelete.id)
+      .subscribe(() => {
+          this.todoList = this.todoList.filter(todo => todo.id !== todoOnDelete.id);
+        }
+      );
+  }
+
+  onCompete(todoOnComplete: Todo): void {
+    this.httpClint
+      .patch<Todo>('http://127.0.0.1:8000/api/update/todo/' + todoOnComplete.id, {
+        isCompleted: !todoOnComplete.isCompleted
+      }).subscribe((updatedTodo: Todo) => {
+      this.todoList = this.todoList.map(todo => todo.id !== updatedTodo.id ? todo : updatedTodo);
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {HttpClientModule} from '@angular/common/http';
+import {FormsModule} from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatExpansionModule} from '@angular/material/expansion';
+import {MatPseudoCheckboxModule} from '@angular/material/core';
+import {MatListModule} from '@angular/material/list';
+import {MatIcon, MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -10,7 +20,17 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    NgbModule,
+    BrowserAnimationsModule,
+    MatGridListModule,
+    MatExpansionModule,
+    MatPseudoCheckboxModule,
+    MatButtonModule,
+    MatListModule,
+    MatIconModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
